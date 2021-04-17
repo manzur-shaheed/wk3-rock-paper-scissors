12b1df6be06712cc50df21a4b0b5aa13d0d85520
ms <shaheed_manzur@yahoo.com>
2021-04-16 23:33:21 -0400

    added event listeners

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,6 +3,22 @@ var ties = 0;
 var losses = 0;
 
 var computerChoice = ["R", "P", "S"]
+var rock = document.querySelector("#rock");
+var paper = document.querySelector("#paper");
+var scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", function(event) {
+    document.getElementById("you").innerHTML = 'R';
+    alert('R');
+});
+
+paper.addEventListener("click", function(event) {
+    alert('P');
+});
+
+scissors.addEventListener("click", function(event) {
+    alert('S');
+});
 
 // get computers choice
 function getComputerChoice() {
@@ -22,5 +38,3 @@ function getUserChoice() {
     }
 }
 
-// Run the game for the first time
-playGame();
